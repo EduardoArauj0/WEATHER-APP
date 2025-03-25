6e5dc9614063aa3c2959b7df0e0bedccc50493e2
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-25 20:50:19 -0300

    feat: criar componente Card para exibir cidade, temperatura e condição do tempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Card from "./components/card";
 
 function App() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex flex-col w-full h-screen items-center justify-center">
       
       <form>
         <input 
@@ -17,6 +18,8 @@ function App() {
           Pesquisar
         </button>
       </form>
+
+      <Card />
     </div>
   );
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Card() {
+    return (
+
+        <div>
+            <div>
+            <span>Nome da Cidade</span>
+            <span>Estado</span>
+            <span>Pais</span>                
+            </div>
+
+            <div>
+            <span>27</span>
+            <span>°C</span>
+            </div>
+
+            <div>
+            <span>icone</span>
+            <span>nublado</span>                
+            </div>
+
+
+
+        </div>
+
+    )
+}
+
+export default Card;
