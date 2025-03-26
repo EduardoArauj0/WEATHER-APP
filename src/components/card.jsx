48964a7d7f44631ffa29d82f00663b69b6859aa2
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-25 21:02:15 -0300

    style: aplicar estilos ao Card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,21 +3,20 @@ import React from "react";
 function Card() {
     return (
 
-        <div>
-            <div>
-            <span>Nome da Cidade</span>
-            <span>Estado</span>
-            <span>Pais</span>                
+        <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
+            <div className="text-center">
+                <span className="block text-xl font-bold text-slate-700">Salvador</span>
+                <span className="text-slate-400 text-sm font-medium">Bahia, Brasil</span>
             </div>
 
-            <div>
-            <span>27</span>
-            <span>°C</span>
+            <div className="font-bold text-slate-700 flex mt-4 mb-2">
+                <span className="text-8xl">27</span>
+                <span className="text-2xl mt-2">°C</span>
             </div>
 
-            <div>
-            <span>icone</span>
-            <span>nublado</span>                
+            <div className="text-center">
+                <span className="block">icone</span>
+                <span className="text-slate-700 font-medium">nublado</span>                
             </div>
 
 
