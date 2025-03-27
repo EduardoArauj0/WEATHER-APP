12d3016acb502d120eb861c4e943ebc1d3ab6fbe
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-26 22:24:04 -0300

    feat: implementar handleSubmit para buscar dados da cidade e exibir resposta da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import fetchData from "./services/api";
+
 import Card from "./components/card";
 
+
 function App() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetchData('Salvador').then( (response) => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
       
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
         type="text" 
         placeholder="Cidade" 
--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,9 @@
     const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
     
     const fetchResponse = await fetch(url);
+    const data = await fetchResponse.json();
+    
+    return data;
  };
 
  export default fetchData;
