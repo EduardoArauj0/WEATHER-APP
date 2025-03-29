77dcda9bb048db146b103e374103fba769892b1f
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-28 21:08:50 -0300

    chore: definir estado inicial com valores padrão para evitar erros de renderização

---
+++ src/services/helpers/initialDate.js
@@ -0,0 +1,16 @@
+const initialDate = {
+    location: {
+        name: '-',
+        region: '-',
+        country: '-',
+    },
+    current: {
+        temp_c: 0,
+        condition: {
+            text: '-',
+            icon: '//cdn.weatherapi.com/weather/64x64/night/353.png',
+        },
+    },
+};
+
+export default initialDate;
